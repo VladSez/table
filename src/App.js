3320257346919a95bcab26052addc64878f9fec7
Vlad Sazon <smallredeagle@gmail.com>
2019-08-13 10:09:58 +0300

    Created basic table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const fakeData = Array(5).fill({
+  orderId: "Vlad",
+  created: 2434,
+  customer: "vladislav sazonius",
+  fulfillment: "Unfulfilled",
+  total: 1000,
+  profit: 999,
+  status: "Paid"
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1>Vlados miros</h1>
+      <div className="overflow">
+       <table className="zui-table zui-table-rounded">
+         <thead>
+        <tr>
+          <th>Order id</th>
+          <th>Created</th>
+          <th>Customer</th>
+          <th>Go</th>
+          <th>Test</th>
+          <th>OMG</th>
+        </tr>
+        </thead>
+        <tbody>
+        {fakeData.map(el => {
+          return (
+            <tr className="row">
+              <td className="cell">{el.orderId}</td>
+              <td>{el.created}</td>
+              <td>{el.customer}</td>
+              <td><input type="text" placeholder="vlados"/></td>
+              <td>{el.fulfillment}</td>
+              <td>{el.fulfillment}</td>
+            </tr>
+          );
+        })}
+        </tbody>
+      </table>
+      </div>
     </div>
   );
 }
